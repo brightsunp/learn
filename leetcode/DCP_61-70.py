37c0196a479f14ef7a033255ee4e09eacdb55414
sunp <sunp@whu.edu.cn>
2019-01-24 09:08:28 +0800

    update

---
+++ leetcode/DCP_61-70.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# coding=utf-8
+'''
+__author__ = 'sunp'
+__date__ = '2019/1/24'
+'''
+
+
+class Solution1(object):
+    '''Google
+
+    Implement integer exponentiation. That is, implement the pow(x, y) function, where x and y are integers and returns x^y.
+    Do this faster than the naive method of repeated multiplication.
+    For example, pow(2, 10) should return 1024.
+    '''
+    pass
