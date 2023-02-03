3ddb9df129e634fbf9dd7307567450547108eab4
Pin Sun <pisun@microsoft.com>
2023-02-03 17:30:24 +0800

    bitwise

--- leetcode_c#/Bitwise/BitwiseMain.cs
+++ leetcode_c#/Bitwise/BitwiseMain.cs
@@ -2,6 +2,7 @@
 * Bitwise operators: &, |, >>, <<, ~, ^.
 * 
 * 1) 1^1=0, 0^1=1; 0^0=0, 1^0=1
+* 2) if n is a power of 2, then: n&(n-1)=0
 */
 using TestMain.Definitions;
 
@@ -17,6 +18,9 @@ namespace TestMain.Bitwise
             AssertEqual(4, SwitchBit(6, 1));
             AssertTrue(CheckIfBitSet(6, 1));
             AssertTrue(!CheckIfBitSet(4, 1));
+
+            AssertEqual(1, CountBits(4));
+            AssertEqual(2, CountBits(6));
         }
 
         private int SetBit(int num, int pos)
@@ -36,7 +40,19 @@ namespace TestMain.Bitwise
 
         private bool CheckIfBitSet(int num, int pos)
         {
+            // or ((num >> pos) & 1) > 0
             return (num & (1 << pos)) > 0;
+        }
+
+        private int CountBits(int num)
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                count += (num & 1);
+                num >>= 1;
+            }
+            return count;
         }
     }
 }
